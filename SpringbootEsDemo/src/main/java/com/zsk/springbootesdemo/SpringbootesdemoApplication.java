d33958d70bd3f199bc0bc83bcda3c326cd4de0d1
shuaikangzhang <303316861@qq.com>
2020-08-20 15:11:04 +0800

    first commit

---
+++ SpringbootEsDemo/src/main/java/com/zsk/springbootesdemo/SpringbootesdemoApplication.java
@@ -0,0 +1,13 @@
+package com.zsk.springbootesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootesdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootesdemoApplication.class, args);
+    }
+
+}
---
+++ SpringbootEsDemo/src/test/java/com/zsk/springbootesdemo/SpringbootesdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsk.springbootesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootesdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
